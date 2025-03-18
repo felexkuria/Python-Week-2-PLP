da1196c06bac75820b36ff961a2b98eb1c71af5c
Felex Kuria <engineerfelex@gmail.com>
2025-03-18 11:01:34 +0300

    added a dictoary example

---
+++ dictonary.py
@@ -0,0 +1,12 @@
+# Create a calendar dictorary wiht events  and days of week and time this event are schedueled
+calendar={
+    "Monday": {"12:00": "Lunch with friends", "14:00": "Meeting with boss"},
+    "Tuesday": {"10:00": "Gym session", "16:00": "Dinner with family"},
+    "Wednesday": {"09:00": "Work project", "18:00": "Movie night"},
+    "Thursday": {"11:00": "Yoga class", "20:00": "Family dinner"},
+    "Friday": {"08:00": "Coffee with friends", "22:00": "Party with friends"},
+    "Saturday": {"13:00": "Dinner with friends", "19:00": "Gym session"},
+    "Sunday": {"14:00": "Family movie night", "21:00": "Dinner with family"}
+
+}
+print(calendar["Monday"]["12:00"])
